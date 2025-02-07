e0e6545a65bfec1c0d7ba535c1ad155957a281fe
hkwln <p.aul@mail.de>
2025-02-07 17:25:28 +0100

    changed name from linkcollector to get_internal_links

--- pathfinder.py
+++ pathfinder.py
@@ -1,3 +1,26 @@
-#there is a function named linkkollektor, 
+#there is a function named linkcollektor, 
 # input url
 # output list of urls
+# aim is to find a path from input to output url from wikipedia
+
+#import the Linkcollector function from LinlCollector.py
+from LinkCollecter import get_internal_links
+
+def find_path(start_url, end_url):
+    visited = set()
+    queue = [(start_url, [start_url])]
+
+    while queue:
+        current_url, path = queue.pop(0)
+        if current_url == end_url:
+            return path
+        if current_url not in visited:
+            visited.add(current_url)
+            for link in get_internal_links(current_url):
+                if link not in visited:
+                    queue.append((link, path + [link]))
+    return None
+
+
+
+
